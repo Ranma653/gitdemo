3bb110d1ea1202260b0fc6ffbf7fb7c0841fc670
Alexis Black <Alexisblack2b2icloud.com>
2023-02-14 20:37:14 -0600

    Initial Commit

---
+++ gitdemo/gitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitdemoApp.swift
+//  gitdemo
+//
+//  Created by Alexis Black on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct gitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
